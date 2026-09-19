cf578e45533b0839538794ee864f2165cbf6a09c
Shejin12 <sergiorodriguez202131443@cunoc.edu.gt>
2026-09-19 00:09:26 -0600

    config

---
+++ mini-erp-backend/src/main/java/com/minierp/mini_erp_backend/MiniErpBackendApplication.java
@@ -0,0 +1,13 @@
+package com.minierp.mini_erp_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniErpBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniErpBackendApplication.class, args);
+	}
+
+}
---
+++ mini-erp-backend/src/test/java/com/minierp/mini_erp_backend/MiniErpBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minierp.mini_erp_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniErpBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
